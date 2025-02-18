f4efd05511b630c8fb1be7037c10e3d90169f252
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2025-02-18 17:12:46 -0500

    se agregaron funciones a los diferentes botones

--- 04-jQuery/js/app.js
+++ 04-jQuery/js/app.js
@@ -10,8 +10,39 @@ $(function () {
 
             $('section.list').append($task)
             $('#input-task').val('')
+            countTasks()
+            countRemains()
         } else {
             alert('Please! Enter a Task')
         }
+
+
+    })
+    // toggle task (remain/done)
+    $('body').on('click', 'input[type=checkbox]', function () {
+        //If Checked
+        if ($(this).prop('checked')) {
+            $(this).parent().addClass('checked')
+        } else {
+            $(this).parent().removeClass('checked')
+        }
+        countRemains()
+    })
+    //remove task
+    $('body').on('click', 'article button', function() {
+        $(this).closest('article').remove()
+        countTasks()
+        countRemains()
     })
 })
+// Count Task
+function countTasks() {
+    $('.num-tasks').text($('article').length)
+    $('.title-tasks').text($('article').length > 1 ? 'Tasks' : 'Task')
+}
+
+function countRemains() {
+    remain = Math.abs($('.checked').length - $('article').length)
+    $('.num-remains').text(Math.abs($('.checked').length - $('article').length))
+    $('.title-remains').text($('.checked').length > 1 ? 'Remains' : 'Remain')
+}
